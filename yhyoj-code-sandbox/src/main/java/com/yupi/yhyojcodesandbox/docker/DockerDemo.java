165e4f7c800529bb38cee0dd039d21d1bbcfaf76
Yhy <yinhongyang546@gmail.com>
2023-10-07 21:30:31 +0800

    我回来立，国庆假期结束，重新回归学习喽

---
+++ yhyoj-code-sandbox/src/main/java/com/yupi/yhyojcodesandbox/docker/DockerDemo.java
@@ -0,0 +1,14 @@
+package com.yupi.yhyojcodesandbox.docker;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.PingCmd;
+import com.github.dockerjava.core.DockerClientBuilder;
+
+public class DockerDemo {
+
+    public static void main(String[] args) {
+        DockerClient dockerClient = DockerClientBuilder.getInstance().build();
+        PingCmd pingCmd = dockerClient.pingCmd();
+        pingCmd.exec();
+    }
+}
